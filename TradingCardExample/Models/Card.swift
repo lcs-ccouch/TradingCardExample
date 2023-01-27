5d5f0405ef49814ec9cebb7fbcb121c73b870e4a
Calum Couch <ccouch@lcs.on.ca>
2023-01-27 14:28:11 -0500

    created structure

---
+++ TradingCardExample/Models/Card.swift
@@ -0,0 +1,19 @@
+//
+//  Card.swift
+//  TradingCardExample
+//
+//  Created by calum couch on 2023-01-27.
+//
+
+import Foundation
+
+    struct HockeyCards {
+    let name: String
+    let goals: Int
+    let assists: Int
+    let GP: Int
+    let height:Double
+    let weight: Double
+    let position: String
+    
+}
